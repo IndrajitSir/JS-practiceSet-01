6dc0d14df75c58549546e633ff535896044f0d79
INDRAJIT MANDAL <149069491+IndrajitSir@users.noreply.github.com>
2023-10-28 20:12:43 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,8 +1,30 @@
-var rect = document.querySelector("#rect");
-window.addEventListener("mousemove",function(details){
-    let yval = gsap.utils.mapRange(0, window.innerHeight, 200, window.innerHeight-200, details.clientY);
-    gsap.to(rect,{
-        top: yval + "px",
-        ease: Power4,
-    });
+var secondRect = document.querySelector("#second");
+
+secondRect.addEventListener("mousemove",function(details){
+    var rectLoc = secondRect.getBoundingClientRect();
+    var insideRectLoc = details.clientY - rectLoc.top;
+
+    if(insideRectLoc < rectLoc.height/2)
+    {
+        var greenColor = gsap.utils.mapRange(0, rectLoc.height/2, 255, 0, insideRectLoc);
+        gsap.to(secondRect,{
+            backgroundColor: `rgb(0,${greenColor},0)`,
+            ease : Power4,
+        });
+    }
+    else{
+        var blueColor = gsap.utils.mapRange(rectLoc.height/2, rectLoc.height, 0, 255, insideRectLoc);
+        gsap.to(secondRect,{
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease : Power4,
+        });
+    }
+
+});
+
+secondRect.addEventListener("mouseleave",function(){
+    gsap.to(secondRect,{
+        backgroundColor : "white",
+    })
+
 });
