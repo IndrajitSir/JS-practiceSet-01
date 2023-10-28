d1ded6be79cb1e592596fc75f878a5a65a3af64c
INDRAJIT MANDAL <149069491+IndrajitSir@users.noreply.github.com>
2023-10-28 20:12:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-window.addEventListener("mousemove",function(details){
-    var rect = document.querySelector("#rect");
-    let xval = gsap.utils.mapRange(0, window.innerWidth, 100+rect.getBoundingClientRect().width/2, window.innerWidth-(100+rect.getBoundingClientRect().width/2), details.clientX);
-    gsap.to('#rect',{
-        left: xval + "px",
-        ease: Power4,
-    });
+var rect = document.querySelector("#center");
+
+rect.addEventListener("mousemove",function(details){
+    var rectLocation = rect.getBoundingClientRect();
+    var insideRectVal = details.clientX - rectLocation.left;
+
+    if(insideRectVal < rectLocation.width/2)
+    {
+        var redColor = gsap.utils.mapRange(0,rectLocation.width/2, 255, 0, insideRectVal);
+        gsap.to(rect,{
+            backgroundColor: `rgb(${redColor}, 0, 0)`,
+            ease : Power4,
+        });
+    }
+    else{
+        var blueColor = gsap.utils.mapRange(rectLocation.width/2, rectLocation.width, 0, 255, insideRectVal);
+        gsap.to(rect,{
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease : Power4,
+        });
+    }
+});
+
+rect.addEventListener("mouseleave",function(){
+     gsap.to(rect,{
+        backgroundColor: "white",
+     })
 });
